b1b10aee597d9a96f25e9c81a6ebd506e76bdb14
MUHAMMEDHAFEEZ <125500179+MUHAMMEDHAFEEZ@users.noreply.github.com>
2023-05-29 00:33:19 +0300

    Update gui.py

--- basmala_bags/build/gui.py
+++ basmala_bags/build/gui.py
@@ -110,6 +110,9 @@ tree.place(x=418,
     width=741,
     height=249)
 
+
+# i1
+
 invoice_list = []
 #cinters defs
 def itemplus():
@@ -139,6 +142,242 @@ my_label.place(x=202,
     y=271,
     width=45,
     height=45)
+#-------------------------------------------------
+#2
+def item1plus():
+    global qty1
+    qty1+=1
+    my_label1.config(text = qty1)
+
+def item1mins():
+    global qty1
+    if(qty1>0):
+         qty1-=1
+    my_label1.config(text = qty1)
+#adding iteam in tree 
+def add_item1():
+    global qty1
+    if(qty1>0):
+         desc = "stitch  galaxy space backpack"
+         price = 170.00
+         line_total= qty1*price
+         invoice_item = [qty1, desc, price, line_total]
+         tree.insert('',0, values=invoice_item)
+         enter_data(qty1,desc,price,line_total)
+         invoice_list.append(invoice_item)
+
+my_label1= Label(window,text = qty1)
+my_label1.place(x=536,
+    y=268,
+    width=45,
+    height=45)
+#-------------------------------------------------
+#3
+
+def item2plus():
+    global qty2
+    qty2+=1
+    my_label2.config(text = qty2)
+
+def item2mins():
+    global qty2
+    if(qty2>0):
+         qty2-=1
+    my_label2.config(text = qty2)
+#adding iteam in tree 
+def add_item2():
+    global qty2
+    if(qty2>0):
+         desc = "stitch  galaxy space backpack"
+         price = 170.00
+         line_total= qty2*price
+         invoice_item = [qty2, desc, price, line_total]
+         tree.insert('',0, values=invoice_item)
+         enter_data(qty2,desc,price,line_total)
+         invoice_list.append(invoice_item)
+
+my_label2= Label(window,text = qty2)
+my_label2.place(x=868,
+    y=269,
+    width=45,
+    height=45)
+#-------------------------------------------------
+#4
+def item3plus():
+    global qty3
+    qty3+=1
+    my_label3.config(text = qty3)
+
+def item3mins():
+    global qty3
+    if(qty3>0):
+         qty3-=1
+    my_label3.config(text = qty3)
+#adding iteam in tree 
+def add_item3():
+    global qty3
+    if(qty3>0):
+         desc = "stitch  galaxy space backpack"
+         price = 170.00
+         line_total= qty3*price
+         invoice_item = [qty3, desc, price, line_total]
+         tree.insert('',0, values=invoice_item)
+         enter_data(qty3,desc,price,line_total)
+         invoice_list.append(invoice_item)
+
+my_label3= Label(window,text = qty3)
+my_label3.place(x=1227,
+    y=273,
+    width=45,
+    height=45)
+#-------------------------------------------------
+#5
+def item4plus():
+    global qty4
+    qty4+=1
+    my_label4.config(text = qty4)
+
+def item4mins():
+    global qty4
+    if(qty4>0):
+         qty4-=1
+    my_label4.config(text = qty4)
+#adding iteam in tree 
+def add_item4():
+    global qty4
+    if(qty4>0):
+         desc = "stitch  galaxy space backpack"
+         price = 170.00
+         line_total= qty4*price
+         invoice_item = [qty4, desc, price, line_total]
+         tree.insert('',0, values=invoice_item)
+         enter_data(qty4,desc,price,line_total)
+         invoice_list.append(invoice_item)
+
+my_label4= Label(window,text = qty4)
+my_label4.place(x=374,
+    y=578,
+    width=45,
+    height=45)
+#-------------------------------------------------
+#6
+def item5plus():
+    global qty5
+    qty5+=1
+    my_label5.config(text = qty5)
+
+def item5mins():
+    global qty5
+    if(qty5>0):
+         qty5-=1
+    my_label5.config(text = qty5)
+#adding iteam in tree 
+def add_item5():
+    global qty5
+    if(qty5>0):
+         desc = "stitch  galaxy space backpack"
+         price = 170.00
+         line_total= qty5*price
+         invoice_item = [qty5, desc, price, line_total]
+         tree.insert('',0, values=invoice_item)
+         enter_data(qty5,desc,price,line_total)
+         invoice_list.append(invoice_item)
+
+my_label5= Label(window,text = qty5)
+my_label5.place(x=701,
+    y=586,
+    width=45,
+    height=45)
+#-------------------------------------------------
+#7
+def item6plus():
+    global qty6
+    qty6+=1
+    my_label6.config(text = qty6)
+
+def item6mins():
+    global qty6
+    if(qty6>0):
+         qty6-=1
+    my_label6.config(text = qty6)
+#adding iteam in tree 
+def add_item6():
+    global qty6
+    if(qty6>0):
+         desc = "stitch  galaxy space backpack"
+         price = 170.00
+         line_total= qty6*price
+         invoice_item = [qty6, desc, price, line_total]
+         tree.insert('',0, values=invoice_item)
+         enter_data(qty6,desc,price,line_total)
+         invoice_list.append(invoice_item)
+
+my_label6= Label(window,text = qty6)
+my_label6.place(x=1084,
+    y=583,
+    width=45,
+    height=45)
+#-------------------------------------------------
+#8
+def item7plus():
+    global qty7
+    qty7+=1
+    my_label7.config(text = qty7)
+
+def item7mins():
+    global qty7
+    if(qty7>0):
+         qty7-=1
+    my_label7.config(text = qty7)
+#adding iteam in tree 
+def add_item7():
+    global qty7
+    if(qty7>0):
+         desc = "stitch  galaxy space backpack"
+         price = 170.00
+         line_total= qty7*price
+         invoice_item = [qty7, desc, price, line_total]
+         tree.insert('',0, values=invoice_item)
+         enter_data(qty7,desc,price,line_total)
+         invoice_list.append(invoice_item)
+
+my_label7 = Label(window,text = qty7)
+my_label7.place(x=191,
+    y=905,
+    width=45,
+    height=45)
+#-------------------------------------------------
+#9
+def item8plus():
+    global qty8
+    qty8+=1
+    my_label8.config(text = qty8)
+
+def item8mins():
+    global qty8
+    if(qty8>0):
+         qty8-=1
+    my_label8.config(text = qty8)
+#adding iteam in tree 
+def add_item8():
+    global qty8
+    if(qty8>0):
+         desc = "stitch  galaxy space backpack"
+         price = 170.00
+         line_total= qty8*price
+         invoice_item = [qty8, desc, price, line_total]
+         tree.insert('',0, values=invoice_item)
+         enter_data(qty8,desc,price,line_total)
+         invoice_list.append(invoice_item)
+
+my_label8 = Label(window,text = qty8)
+my_label8.place(x=1272,
+    y=894,
+    width=45,
+    height=45)
+#-------------------------------------------------
+
+
 
 image_image_1 = PhotoImage(
     file=relative_to_assets("image_1.png"))
@@ -154,7 +393,7 @@ button_1 = Button(
     image=button_image_1,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_1 clicked"),
+    command=lambda: add_item(),
     relief="flat"
 )
 button_1.place(
@@ -170,7 +409,7 @@ button_2 = Button(
     image=button_image_2,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_2 clicked"),
+    command=lambda: add_item4(),
     relief="flat"
 )
 button_2.place(
@@ -186,7 +425,7 @@ button_3 = Button(
     image=button_image_3,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_3 clicked"),
+    command=lambda: add_item7(),
     relief="flat"
 )
 button_3.place(
@@ -202,7 +441,7 @@ button_4 = Button(
     image=button_image_4,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_4 clicked"),
+    command=lambda: add_item1(),
     relief="flat"
 )
 button_4.place(
@@ -218,7 +457,7 @@ button_5 = Button(
     image=button_image_5,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_5 clicked"),
+    command=lambda: add_item5(),
     relief="flat"
 )
 button_5.place(
@@ -234,7 +473,7 @@ button_6 = Button(
     image=button_image_6,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_6 clicked"),
+    command=lambda: add_item8(),
     relief="flat"
 )
 button_6.place(
@@ -250,7 +489,7 @@ button_7 = Button(
     image=button_image_7,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_7 clicked"),
+    command=lambda: add_item2(),
     relief="flat"
 )
 button_7.place(
@@ -266,7 +505,7 @@ button_8 = Button(
     image=button_image_8,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_8 clicked"),
+    command=lambda: add_item3(),
     relief="flat"
 )
 button_8.place(
@@ -282,7 +521,7 @@ button_9 = Button(
     image=button_image_9,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_9 clicked"),
+    command=lambda: add_item6(),
     relief="flat"
 )
 button_9.place(
@@ -298,7 +537,7 @@ button_10 = Button(
     image=button_image_10,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_10 clicked"),
+    command=lambda: itemmins(),
     relief="flat"
 )
 button_10.place(
@@ -314,7 +553,7 @@ button_11 = Button(
     image=button_image_11,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_11 clicked"),
+    command=lambda: item1mins(),
     relief="flat"
 )
 button_11.place(
@@ -330,7 +569,7 @@ button_12 = Button(
     image=button_image_12,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_12 clicked"),
+    command=lambda: item2mins(),
     relief="flat"
 )
 button_12.place(
@@ -346,7 +585,7 @@ button_13 = Button(
     image=button_image_13,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_13 clicked"),
+    command=lambda: item3mins(),
     relief="flat"
 )
 button_13.place(
@@ -362,7 +601,7 @@ button_14 = Button(
     image=button_image_14,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_14 clicked"),
+    command=lambda: item5mins(),
     relief="flat"
 )
 button_14.place(
@@ -378,7 +617,7 @@ button_15 = Button(
     image=button_image_15,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_15 clicked"),
+    command=lambda: item4mins(),
     relief="flat"
 )
 button_15.place(
@@ -394,7 +633,7 @@ button_16 = Button(
     image=button_image_16,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_16 clicked"),
+    command=lambda: item7mins(),
     relief="flat"
 )
 button_16.place(
@@ -410,7 +649,7 @@ button_17 = Button(
     image=button_image_17,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_17 clicked"),
+    command=lambda: item8mins(),
     relief="flat"
 )
 button_17.place(
@@ -426,7 +665,7 @@ button_18 = Button(
     image=button_image_18,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_18 clicked"),
+    command=lambda: item6mins(),
     relief="flat"
 )
 button_18.place(
@@ -450,7 +689,7 @@ button_19 = Button(
     image=button_image_19,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_19 clicked"),
+    command=lambda: itemplus(),
     relief="flat"
 )
 button_19.place(
@@ -466,7 +705,7 @@ button_20 = Button(
     image=button_image_20,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_20 clicked"),
+    command=lambda: item1plus(),
     relief="flat"
 )
 button_20.place(
@@ -482,7 +721,7 @@ button_21 = Button(
     image=button_image_21,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_21 clicked"),
+    command=lambda: item2plus(),
     relief="flat"
 )
 button_21.place(
@@ -498,7 +737,7 @@ button_22 = Button(
     image=button_image_22,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_22 clicked"),
+    command=lambda: item3plus(),
     relief="flat"
 )
 button_22.place(
@@ -514,7 +753,7 @@ button_23 = Button(
     image=button_image_23,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_23 clicked"),
+    command=lambda: item6plus(),
     relief="flat"
 )
 button_23.place(
@@ -530,7 +769,7 @@ button_24 = Button(
     image=button_image_24,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_24 clicked"),
+    command=lambda: item5plus(),
     relief="flat"
 )
 button_24.place(
@@ -546,7 +785,7 @@ button_25 = Button(
     image=button_image_25,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_25 clicked"),
+    command=lambda: item4plus(),
     relief="flat"
 )
 button_25.place(
@@ -562,7 +801,7 @@ button_26 = Button(
     image=button_image_26,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_26 clicked"),
+    command=lambda: item7plus(),
     relief="flat"
 )
 button_26.place(
@@ -578,7 +817,7 @@ button_27 = Button(
     image=button_image_27,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_27 clicked"),
+    command=lambda: item8plus(),
     relief="flat"
 )
 button_27.place(
@@ -594,7 +833,7 @@ button_28 = Button(
     image=button_image_28,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_28 clicked"),
+    command=lambda: new_invoice(),
     relief="flat"
 )
 button_28.place(
@@ -610,7 +849,7 @@ button_29 = Button(
     image=button_image_29,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_29 clicked"),
+    command=lambda: generate_invoice(),
     relief="flat"
 )
 button_29.place(
